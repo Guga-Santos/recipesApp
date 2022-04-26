700a0174b5ac756aae2e3634887a4a3849beb54a
Breno L. Lopes <brernolopes@hotmail.com>
2022-04-26 17:01:12 -0300

    Requisitos 2 ao 5 ok

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import React from 'react';
-import { Switch, Router } from 'react-router-dom';
-import './App.css';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import rockGlass from './images/rockGlass.svg';
+import Login from './pages/Login';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Switch>
-      <Router exact path="/" />
-      <Router path="/food" />
-      <Router path="/drinks" />
-      <Router path="/foods/{id-da-receita}" />
-      <Router path="/drinks/{id-da-receita}" />
-      <Router path="/foods/{id-da-receita}/in-progress" />
-      <Router path="/drinks/{id-da-receita}/in-progress" />
-      <Router path="/explore" />
-      <Router path="/explore/foods" />
-      <Router path="/explore/drinks" />
-      <Router path="/explore/foods/ingredients" />
-      <Router path="/explore/drinks/ingredients" />
-      <Router path="/explore/foods/nationalities" />
-      <Router path="/profile" />
-      <Router path="/done-recipes" />
-      <Router path="/favorite-recipes" />
-      <div className="meals">
-        <span className="logo">TRYBE</span>
-        <object
-          className="rocksGlass"
-          type="image/svg+xml"
-          data={ rockGlass }
-        >
-          Glass
-        </object>
-      </div>
-    </Switch>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/food" />
+        <Route path="/drinks" />
+        <Route path="/foods/{id-da-receita}" />
+        <Route path="/drinks/{id-da-receita}" />
+        <Route path="/foods/{id-da-receita}/in-progress" />
+        <Route path="/drinks/{id-da-receita}/in-progress" />
+        <Route path="/explore" />
+        <Route path="/explore/foods" />
+        <Route path="/explore/drinks" />
+        <Route path="/explore/foods/ingredients" />
+        <Route path="/explore/drinks/ingredients" />
+        <Route path="/explore/foods/nationalities" />
+        <Route path="/profile" />
+        <Route path="/done-recipes" />
+        <Route path="/favorite-recipes" />
+        <div className="meals">
+          <span className="logo">TRYBE</span>
+          <object
+            className="rocksGlass"
+            type="image/svg+xml"
+            data={ rockGlass }
+          >
+            Glass
+          </object>
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Login() {
+function Login() {
+  const [passwordValue, setPasswordValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [enableButton, setEnableButton] = useState(true);
+
+  const activeButton = () => {
+    const mingLength = 6;
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]/i;
+    const checkEmail = emailRegex.test(emailValue);
+    const passwordLength = passwordValue.length > mingLength;
+    if (checkEmail && passwordLength) {
+      setEnableButton(false);
+    } else { setEnableButton(true); }
+  };
+
+  const changePaswordValue = (target) => {
+    setPasswordValue(target.value);
+    activeButton();
+  };
+
+  const changeEmailValue = ({ target }) => {
+    setEmailValue(target.value);
+    activeButton();
+  };
+
   return (
-    <div className="login-container">
-      <label htmlFor="input-email">
-        Email:
-        <input
-          type="email"
-          id="input-email"
-          data-testid="email-input"
-        />
-      </label>
-      <label htmlFor="input-email">
-        Password:
-        <input
-          type="email"
-          id="input-password"
-          data-testid="password-input"
-        />
-      </label>
+    <section>
+      <input
+        type="text"
+        data-testid="email-input"
+        value={ emailValue }
+        onChange={ changeEmailValue }
+      />
+      <input
+        type="text"
+        data-testid="password-input"
+        onChange={ ({ target }) => { changePaswordValue(target); } }
+        value={ passwordValue }
+      />
       <button
         type="button"
         data-testid="login-submit-btn"
+        disabled={ enableButton }
       >
-        Enter
-
+        Login
       </button>
-    </div>
+    </section>
   );
 }
+
+export default Login;

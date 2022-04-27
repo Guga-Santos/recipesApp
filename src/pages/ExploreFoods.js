6f6cbc860d9e84df0a35df441062475e6468d305
Raphael Freitas <raphael.soarescivil@gmail.com>
2022-04-27 09:10:52 -0300

    Adicionando o Footer as paginas

--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Footer from '../components/footer';
 import Header from '../components/Header';
 
 export default function ExploreFoods() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Footer />
+    </div>
   );
 }

18476e627e027ea7b538a68e2857c61a0320973e
thiago <ts_muniz@outlook.com>
2022-04-28 18:32:09 -0300

    iniciando os trabalhos

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -3,6 +3,9 @@ import Header from '../components/Header';
 
 export default function DoneRecipes() {
   return (
-    <Header title="Done Recipes" hasSearch={ false } />
+    <>
+      <Header title="Done Recipes" hasSearch={ false } />
+      <div />
+    </>
   );
 }

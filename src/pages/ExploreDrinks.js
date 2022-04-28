a986e8a5680866e895122219bf26e7f46d60c2c6
GugaSantos <guga.santos.guga@gmail.com>
2022-04-28 01:57:05 -0300

    Requisitos 67 ao 76 ok

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/footer';
 import Header from '../components/Header';
+import fetchRandom from '../services/fetchRandom';
 
 export default function ExploreDrinks() {
+  const history = useHistory();
+
+  const handleSurprise = async () => {
+    const data = await fetchRandom('cocktail');
+    history.push(`/drinks/${data.drinks[0].idDrink}`);
+  };
+
   return (
     <div>
       <Header title="Explore Drinks" hasSearch={ false } />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/drinks/ingredients') }
+      >
+        By Ingredient
+
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => handleSurprise() }
+      >
+        Surprise me!
+
+      </button>
       <Footer />
     </div>
   );
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/footer';
 import Header from '../components/Header';
+import fetchRandom from '../services/fetchRandom';
 
 export default function ExploreFoods() {
+  const history = useHistory();
+
+  const handleSurprise = async () => {
+    const data = await fetchRandom('meal');
+    history.push(`/foods/${data.meals[0].idMeal}`);
+  };
+
   return (
     <div>
       <Header title="Explore Foods" hasSearch={ false } />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/foods/ingredients') }
+      >
+        By Ingredient
+
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+        onClick={ () => history.push('/explore/foods/nationalities') }
+      >
+        By Nationality
+
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => handleSurprise() }
+      >
+        Surprise me!
+
+      </button>
       <Footer />
     </div>
   );

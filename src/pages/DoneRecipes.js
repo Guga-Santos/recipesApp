9b666fe3bdcd95eb785216718d741f03894e995d
thiago <ts_muniz@outlook.com>
2022-04-28 21:29:53 -0300

    cards para exibição das receitas feitas prontos

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,11 +1,71 @@
-import React from 'react';
+/* eslint-disable sonarjs/no-duplicate-string */
+import React, { useEffect, useState } from 'react';
+import DoneRecipesCard from '../components/DoneRecipesCard';
+import DoneRecipesFilters from '../components/DoneRecipesFilters';
 import Header from '../components/Header';
 
+const mock = [
+  {
+    id: '52804',
+    type: 'Food',
+    nationality: 'Canadian',
+    category: 'Miscellaneous',
+    alcoholicOrNot: '',
+    name: 'Poutine',
+    image: 'https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg',
+    doneDate: '26/04/2022',
+    tags: ['UnHealthy', 'Speciality', 'HangoverFood'],
+  },
+  {
+    id: '178333',
+    type: 'Drink',
+    nationality: '',
+    category: 'Miscellaneous',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Raspberry Julep',
+    image: 'https://www.thecocktaildb.com/images/media/drink/hyztmx1598719265.jpg',
+    doneDate: '26/04/2022',
+    tags: [],
+  },
+  {
+    id: '178333',
+    type: 'Drink',
+    nationality: '',
+    category: 'Miscellaneous',
+    alcoholicOrNot: 'Non alcoholic',
+    name: 'Raspberry Julep',
+    image: 'https://www.thecocktaildb.com/images/media/drink/hyztmx1598719265.jpg',
+    doneDate: '26/04/2022',
+    tags: [],
+  },
+];
 export default function DoneRecipes() {
+  const [doneRecipesInfo, setDoneRecipesInfo] = useState(mock);
+  const [filter, setFilter] = useState('');
+  console.log(filter);
+  console.log(doneRecipesInfo);
+  useEffect(() => {
+    setDoneRecipesInfo(mock);
+  }, []);
   return (
     <>
       <Header title="Done Recipes" hasSearch={ false } />
-      <div />
+      <DoneRecipesFilters setFilter={ setFilter } />
+      {doneRecipesInfo.map((recipeInfo, index) => (
+        <DoneRecipesCard
+          key={ `${index}${recipeInfo.name}` }
+          recipeImage={ recipeInfo.image }
+          recipeName={ recipeInfo.name }
+          recipeCategory={ recipeInfo.category }
+          recipeNationality={ recipeInfo.nationality }
+          recipeDate={ recipeInfo.doneDate }
+          recipeTag={ recipeInfo.tags }
+          recipeIndex={ index }
+          recipeType={ recipeInfo.type }
+          isAlcoholic={ recipeInfo.alcoholicOrNot }
+        />
+      ))}
+
     </>
   );
 }

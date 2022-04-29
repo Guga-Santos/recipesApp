050cb1811c922060a4f736866222742fcde1250d
thiago <ts_muniz@outlook.com>
2022-04-28 22:17:18 -0300

    mock retirado, utilizando o localstorage para testes

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -4,48 +4,50 @@ import DoneRecipesCard from '../components/DoneRecipesCard';
 import DoneRecipesFilters from '../components/DoneRecipesFilters';
 import Header from '../components/Header';
 
-const mock = [
-  {
-    id: '52804',
-    type: 'Food',
-    nationality: 'Canadian',
-    category: 'Miscellaneous',
-    alcoholicOrNot: '',
-    name: 'Poutine',
-    image: 'https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg',
-    doneDate: '26/04/2022',
-    tags: ['UnHealthy', 'Speciality', 'HangoverFood'],
-  },
-  {
-    id: '178333',
-    type: 'Drink',
-    nationality: '',
-    category: 'Miscellaneous',
-    alcoholicOrNot: 'Alcoholic',
-    name: 'Raspberry Julep',
-    image: 'https://www.thecocktaildb.com/images/media/drink/hyztmx1598719265.jpg',
-    doneDate: '26/04/2022',
-    tags: [],
-  },
-  {
-    id: '178333',
-    type: 'Drink',
-    nationality: '',
-    category: 'Miscellaneous',
-    alcoholicOrNot: 'Non alcoholic',
-    name: 'Raspberry Julep',
-    image: 'https://www.thecocktaildb.com/images/media/drink/hyztmx1598719265.jpg',
-    doneDate: '26/04/2022',
-    tags: [],
-  },
-];
+// const mock = [
+//   {
+//     id: '52804',
+//     type: 'Food',
+//     nationality: 'Canadian',
+//     category: 'Miscellaneous',
+//     alcoholicOrNot: '',
+//     name: 'Poutine',
+//     image: 'https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg',
+//     doneDate: '26/04/2022',
+//     tags: ['UnHealthy', 'Speciality', 'HangoverFood'],
+//   },
+//   {
+//     id: '178333',
+//     type: 'Drink',
+//     nationality: '',
+//     category: 'Miscellaneous',
+//     alcoholicOrNot: 'Alcoholic',
+//     name: 'Raspberry Julep',
+//     image: 'https://www.thecocktaildb.com/images/media/drink/hyztmx1598719265.jpg',
+//     doneDate: '26/04/2022',
+//     tags: [],
+//   },
+//   {
+//     id: '178333',
+//     type: 'Drink',
+//     nationality: '',
+//     category: 'Miscellaneous',
+//     alcoholicOrNot: 'Non alcoholic',
+//     name: 'Raspberry Julep',
+//     image: 'https://www.thecocktaildb.com/images/media/drink/hyztmx1598719265.jpg',
+//     doneDate: '26/04/2022',
+//     tags: [],
+//   },
+// ];
 export default function DoneRecipes() {
-  const [doneRecipesInfo, setDoneRecipesInfo] = useState([]);
+  const recipesOnStorage = JSON.parse(localStorage.getItem('doneRecipes'));
+  const [doneRecipesInfo, setDoneRecipesInfo] = useState(recipesOnStorage);
   const [recipesFiltered, setRecipesFiltered] = useState([]);
   const [filter, setFilter] = useState('All');
   useEffect(() => {
-    setDoneRecipesInfo(mock);
+    setDoneRecipesInfo(recipesOnStorage);
     setRecipesFiltered(filter);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const filterRecipesBytype = (filterByType) => {

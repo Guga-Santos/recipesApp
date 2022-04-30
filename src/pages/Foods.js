77283d0d5194fc8c3d585a754884f6a69d73dc03
GugaSantos <guga.santos.guga@gmail.com>
2022-04-30 02:34:53 -0300

    Requisito 15 passando no cypress

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -16,6 +16,8 @@ export default function Foods(props) {
     ingredient,
   } = contexto;
 
+  // Teste
+
   useEffect(() => {
     const get = JSON.parse(localStorage.getItem('favoriteRecipes'));
 
